58379eddc9a2dcaf1b18572f9bc9f82f3000b040
WatchingBoss <opil94@yandex.ru>
2018-07-22 18:40:37 +0300

    Add flycheck package and addition packages for c/c++ and shell

--- emacs/init.el
+++ emacs/init.el
@@ -18,7 +18,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(web-mode php-mode zenburn-theme org counsel tabbar org-bullets yasnippet-snippets multiple-cursors yasnippet auto-complete try use-package usepackage which-key))))
+	(flycheck-checkbashisms flycheck-pkg-config flycheck web-mode php-mode zenburn-theme org counsel tabbar org-bullets yasnippet-snippets multiple-cursors yasnippet auto-complete try use-package usepackage which-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
