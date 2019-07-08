a9c8385d18493ba578a3d5652bb7eaabd85998aa
WatchingBoss <opil94@yandex.ru>
2019-07-08 22:19:48 +0300

    Backup last emacs configs

--- emacs/init.el
+++ emacs/init.el
@@ -11,29 +11,25 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   (quote
-	("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-	(ox-hugo org-ac ox-pandoc ox-reveal flycheck-pychekers glsl-mode flycheck-pycheckers flyckeck-pycheckers clang-format neotree flycheck-rust racer rust-mode ycmd-eldoc flycheck-ycmd company-ycmd ycmd flycheck-irony irony-eldoc company-anaconda company-jedi company-irony markdown-mode go-eldoc go-mode flycheck-gometalinter smartparens company csharp-mode org-gcal monokai-theme expand-region hungry-delete flycheck-checkbashisms flycheck-pkg-config flycheck web-mode php-mode zenburn-theme org counsel tabbar org-bullets yasnippet-snippets multiple-cursors yasnippet try use-package usepackage which-key))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package org-tempo)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
 (server-start)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(yasnippet-snippets yasnippet ccls helm-ls-git helm-swoop helm company-lsp lsp-ui lsp-mode use-package org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
