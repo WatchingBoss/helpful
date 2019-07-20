558370e42428073d058cb02cdf74776fdbfd6bfc
WatchingBoss <opil94@yandex.ru>
2019-07-20 15:57:26 +0300

    Last emacs backup

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,10 @@
 ;; start package.el
 (require 'package)
-;; add MELPA to repository list
-(add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/") t)
+
+;; add repository list
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/" ) t)
+
 ;; initialize package.el
 (package-initialize)
 
@@ -11,14 +13,14 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package org-tempo)
+;;(use-package org-tempo)
+(use-package org :ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
 (server-start)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(yasnippet-snippets yasnippet ccls helm-ls-git helm-swoop helm company-lsp lsp-ui lsp-mode use-package org))))
+	(spinner lsp-mode clang-format org-ac yasnippet-snippets which-key use-package smartparens rust-mode neotree multiple-cursors monokai-theme monokai-pro-theme markdown-mode hungry-delete helm-swoop helm-ls-git go-mode glsl-mode flycheck expand-region emmet-mode csharp-mode company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
