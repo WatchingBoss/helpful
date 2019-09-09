73016453a3f26b31d7056f33b90963f508331668
WatchingBoss <opil94@yandex.ru>
2019-09-09 19:55:05 +0300

    Last emacs backup

--- emacs/init.el
+++ emacs/init.el
@@ -30,7 +30,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(htmlize jsonrpc eglot company-quickhelp helm-flx company-statistics helm-system-packages helm-descbinds flx helm-swoop org-bullets flycheck emmet-mode yasnippet-snippets yasnippet company rg ox-hugo ox-pandoc markdown-mode glsl-mode go-mode rust-mode csharp-mode clang-format smartparens expand-region hungry-delete multiple-cursors reverse-im treemacs-magit treemacs-icons-dired treemacs-projectile treemacs helm-ls-git which-key monokai-theme use-package))))
+	(neotree ccls lsp-mode helm-lsp htmlize jsonrpc eglot company-quickhelp helm-flx company-statistics helm-system-packages helm-descbinds flx helm-swoop org-bullets flycheck emmet-mode yasnippet-snippets yasnippet company rg ox-hugo ox-pandoc markdown-mode glsl-mode go-mode rust-mode csharp-mode clang-format smartparens expand-region hungry-delete multiple-cursors reverse-im treemacs-magit treemacs-icons-dired treemacs-projectile treemacs helm-ls-git which-key monokai-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
