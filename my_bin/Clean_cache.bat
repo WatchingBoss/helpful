28ba8d0e1f7cba12b80427e3c61bb20df3eef2c3
WatchingBoss <opil94@yandex.ru>
2021-01-23 01:15:01 +0300

    Exported bin folder

---
+++ my_bin/Clean_cache.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem set cashe_dir_1=""
+echo %cashe_dir_1%
+
+rem del /F /S /Q %cashe_dir_1%\*
---
+++ my_bin/export.bat
@@ -0,0 +1,8 @@
+@echo off
+
+copy "%HOME%\AppData\Roaming\Adobe\Adobe Photoshop 2020\Adobe Photoshop 2020 Settings\WorkSpaces\Action_layout.psw" F:\Work\helpful\photoshop\
+echo Photoshop WorkSpaces copied
+
+echo "%HOME%\bin\*"
+copy "%HOME%\bin\*" F:\Work\helpful\my_bin\
+echo bin folder copied
